8bffff0e4394f279d06ce7f3bdcbb5630f57dd2d
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-12-08 19:06:29 +0530

    highlighting active Player's timer

--- src/features/game/timer/Timer.tsx
+++ src/features/game/timer/Timer.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { getAllGames } from "../../../redux/games";
 import { useSelector } from "react-redux";
-// import { TimerBase } from "../../../common/model";
+import { getCurrentPlayerId } from "../../../redux/currentPlayer";
 
 const Timer = (props: any) => {
   const game = useSelector(getAllGames)[props.gameId];
   const timers = game.timers;
-
   return (
     <div className="flex flex-col">
-      {Object.values(timers).map((timer: any) => {
-        return <text>{Math.max(0, timer.timeLeft / 1000)}</text>;
+      {Object.entries(timers).map(([key, timer]: any) => {
+        return (
+          <text className={key === game.turn ? "text-red-900" : ""}>
+            {Math.max(0, timer.timeLeft / 1000)}
+          </text>
+        );
       })}
     </div>
   );

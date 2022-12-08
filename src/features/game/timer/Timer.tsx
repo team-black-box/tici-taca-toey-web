864a00390d2dcf6f6c83fffb7e507d2420986da5
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-12-08 17:59:03 +0530

    timer stopping at 0

--- src/features/game/timer/Timer.tsx
+++ src/features/game/timer/Timer.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { getAllGames } from "../../../redux/games";
 import { useSelector } from "react-redux";
+// import { TimerBase } from "../../../common/model";
 
 const Timer = (props: any) => {
   const game = useSelector(getAllGames)[props.gameId];
   const timers = game.timers;
+
   return (
     <div className="flex flex-col">
       {Object.values(timers).map((timer: any) => {
-        return <text>{timer.timeLeft}</text>;
+        return <text>{Math.max(0, timer.timeLeft / 1000)}</text>;
       })}
     </div>
   );

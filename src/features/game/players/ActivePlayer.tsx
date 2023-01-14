be65712cdb171885967c265459efe478a30f0a29
Aditya Vardhan <adtyavrdhn@gmail.com>
2023-01-14 15:27:17 +0530

    minor fixes

--- src/features/game/players/ActivePlayer.tsx
+++ src/features/game/players/ActivePlayer.tsx
@@ -15,22 +15,22 @@ const ActivePlayer = ({ playerId, players, turn }: ActivePlayer) => {
   const symbol = getSymbol(playerId, players);
   const activeGameId: string = useSelector(getActiveGameId);
   const game = useSelector(getAllGames)[activeGameId];
-  const timers = game.timers;
+  const timers = game?.timers;
 
   return (
     <div
       className={`flex flex-row border-2 border-slate-800 items-center justify-center gap-2 text-lg rounded-l-2xl rounded-r-2xl font-bold`}
     >
       <div
-        className={`text-center p-3 text-black text-sm bg-${symbol.color}-500 rounded-l-2xl`}
+        className={`text-center p-3 text-black text-sm bg-${symbol?.color}-500 rounded-l-2xl`}
       >
         {symbol.symbol}
       </div>
       <div
-        className={`text-sm p-3 uppercase bg-white text-${symbol.color}-900 rounded-r-2xl`}
+        className={`text-sm p-3 uppercase bg-white text-${symbol?.color}-900 rounded-r-2xl`}
       >
         <span>
-          {formattedTime(timers[playerId].timeLeft)}
+          {formattedTime(timers[playerId]?.timeLeft)}
           <span className="text-xs">s</span>
         </span>
       </div>
